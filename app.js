d783874c1394e425c5a3e98bc193bd3699eccada
Maggi <gila44dri@hotmail.com>
2022-09-21 19:06:05 -0300

    arreglos detalles

---
+++ app.js
@@ -0,0 +1,3 @@
+gsap.from('.title', {opacity: 0, duration: 1, delay: 1, y:30, stagger: 0.2})
+gsap.from('.subtitle', {opacity: 0, duration: 1, delay: 0.5, y:30, stagger: 0.2})
+gsap.from('.figure', {opacity: 0, duration: 1, delay: 0.5, y:10})
