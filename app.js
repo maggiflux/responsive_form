9b4fa9e41c0f2a6c1b37cfe7bfe71238fa374c91
Maggi <gila44dri@hotmail.com>
2022-10-14 11:38:39 -0300

    js y prefijos

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-gsap.from('.title', {opacity: 0, duration: 1, delay: 1, y:30, stagger: 0.2})
-gsap.from('.subtitle', {opacity: 0, duration: 1, delay: 0.5, y:30, stagger: 0.2})
-gsap.from('.figure', {opacity: 0, duration: 1, delay: 0.5, y:10})
+const btnSubmit = document.querySelector('.btn-send');
+const mainContact = document.querySelector('.subscription__done');
+
+const all = document.querySelector('.main__form');
+const inputEmail = document.querySelector('#input__email');
+const parrafo = document.querySelector('#warnings')
+
+all.addEventListener("submit", handlerSubmit) 
+function handlerSubmit(event) {
+    event.preventDefault()  
+
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    
+    if(!regexEmail.test(inputEmail.value) === true ) {
+        parrafo.innerHTML = `El email no es válido <br>`
+    } else { 
+        const form = new FormData(this)
+    
+        console.log(form.get('name'))
+        console.log(form.get('lastname'))
+        console.log(form.get('phone'))
+        console.log(form.get('email'))
+
+        all.style.display = 'none';
+        let subsDone = document.createElement('div');
+        subsDone.innerHTML = '<h3>Gracias por subscribirte</h3>'
+        subsDone.style.padding = '2rem'
+        mainContact.appendChild(subsDone)
+    }
+    console.log(regexEmail.test(inputEmail.value))
+}
+
+
+
+
+
